3bc3f68942f66e10c8b317c925d58f045522e540
ayushkaushal1522 <ayushkaushal1522@gmail.com>
2023-07-29 11:04:00 +0530

    first commit

---
+++ levelorder.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+// time and space complexity is given by O(n).
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        Node(int val){
+            data=val;
+            left=NULL;
+            right=NULL;
+        }    
+};
+vector<vector<int>> levelorder(Node* root){
+    queue<Node*> q;
+    vector<vector<int>> ans;
+    q.push(root);
+    while(!q.empty()){
+        int n=q.size();
+        vector<int> level;
+        for(int i=0;i<n;i++){
+            Node* temp=q.front();
+            q.pop();
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+            level.push_back(temp->data);
+        }
+        ans.push_back(level);
+        
+       
+    }
+    return ans;
+}
+
+int main(){
+
+
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+    vector<vector<int>> res=levelorder(root);
+    for(int i=0;i<res.size();i++){
+        for(int j=0;j<res[i].size();j++){
+            cout<<res[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
